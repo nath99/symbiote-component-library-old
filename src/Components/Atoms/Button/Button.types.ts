4c1e73f944397c070380eba58957dc7978649643
Nathan Barry <nathanb@symbiote.com.au>
2021-07-06 09:38:13 +1000

    Feat(base styles) Add base styles for components

---
+++ src/Components/Atoms/Button/Button.types.ts
@@ -0,0 +1,6 @@
+export interface ButtonProps {
+    type: 'primary'|'secondary'|'tertiary',
+    size: 'lg'|'md'|'sm'|'xs',
+    style: 'outline-light'|'outline-dark'|'cta',
+    enabled: boolean
+}
