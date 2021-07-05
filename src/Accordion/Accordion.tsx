1a44280e0aab6470cb0705d6bcb78a32a2076eba
Nathan Barry <nathanb@symbiote.com.au>
2021-07-05 11:29:29 +1000

    mod(Accordion) Change Accordion export to class

--- src/Accordion/Accordion.tsx
+++ src/Accordion/Accordion.tsx
@@ -4,21 +4,22 @@ import { AccordionProps } from "./Accordion.types";
 
 // import "./Accordion.scss";
 
-const Accordion: React.FC<AccordionProps> = ({ accordionContent }) => (
-  <div
-    data-testid="accordion"
-    className={`accordion`}
-  >
-    { accordionContent.map((item, index) => (
-        <div key={index} className="tab">
-            <input type="checkbox" id={`accordion-${index}`}></input>
-            <label className="tab-label" htmlFor={`accordion-${index}`}>{item.title}</label>
-            <div className="tab-content">
-                <p>{item.content}</p>
-            </div>
-        </div>
-    )) }
-  </div>
-);
-
-export default Accordion;
+export default class Accordion extends React.Component<AccordionProps> {
+  render() {
+      return (
+          <div
+            data-testid="accordion"
+            className="accordion">
+              {this.props.accordionContent.map((item, index) => (
+                  <div key={index} className="tab">
+                      <input type="checkbox" id={`accordion-${index}`}></input>
+                      <label className="tab-label" htmlFor={`accordion-${index}`}>{item.title}</label>
+                  <div className="tab-content">
+                          <p>{item.content}</p>
+                  </div>
+              </div>
+              ))}
+          </div>
+      )
+  }
+}
