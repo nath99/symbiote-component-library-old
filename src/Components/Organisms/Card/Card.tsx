b9ac3706ed23265ed05a42e6d73b6cbaa41d70f1
Amir Sarkis <amir@symbiote.com.au>
2021-07-06 21:41:06 +1000

    Added Card component

---
+++ src/Components/Organisms/Card/Card.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import "../../../styles/organisms/Card.scss";
+
+interface Props {
+    image?: string
+    fullImage: boolean
+    wide?: boolean
+    date?: Date
+    topic?: string
+    heading?: string
+    body?: string
+    callToActionText?
+    callToActionLink?
+}
+
+export default class Card extends React.Component<Props> {
+    getMonthString(d: Date): string {
+        const days = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+        return days[d.getMonth()];
+    }
+
+    render() {
+        const { image,
+            fullImage,
+            wide,
+            date,
+            topic,
+            heading,
+            body,
+            callToActionText,
+            callToActionLink
+        } = this.props
+
+        return (
+            <div className={`col-xs-12 ${wide ? "col-sm-8" : "col-sm-4"}`}>
+                <div className={`card ${(image && fullImage) ? "full-image-card " : image ? "" : "no-image "} ${wide ? "wide-card" : ""}`}>
+
+                    <div className={`${wide ? "" : "card-wrapper"}`} style={wide ? {} : {
+                        backgroundImage: `url(${image})`
+                    }}>
+                        {/* Optional date */}
+                        {date &&
+                            <div className="date">
+                                <span className="day">{date.getDay}</span>
+                                <span className="month">{this.getMonthString(date)}</span>
+                                <span className="year">{date.getFullYear}</span>
+                            </div>
+                        }
+                        <div className="content">
+                            {topic && <p className="inverted">Topic</p>}
+                            {heading && <h3 className="inverted">{heading}</h3>}
+                            {body && <p className="inverted">{body}</p>}
+                            {callToActionText && callToActionLink &&
+                                <div className="button-container">
+                                    <button className="cta inverted">Read more</button>
+                                </div>
+                            }
+                        </div>
+
+                        {wide && image &&
+                            <div className="card-image" style={{
+                                backgroundImage: `url(${image})`
+                            }}></div>
+                        }
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import Button from "./Components/Atoms/Button/Button";
 import Accordion from "./Components/Molecules/Accordion/Accordion";
 import Breadcrumbs from "./Components/Molecules/Breadcrumbs/Breadcrumbs";
+import Card from "./Components/Organisms/Card/Card";
 
-export { Accordion, Button, Breadcrumbs };
+export { Accordion, Button, Breadcrumbs, Card };
