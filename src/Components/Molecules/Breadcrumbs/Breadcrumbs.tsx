07685126ddae3bd2c64cee852aa5da14615dc5e1
Amir Sarkis <amir@symbiote.com.au>
2021-07-06 13:53:48 +1000

    Added Breadcrumbs component + tweaked Accordion component

---
+++ src/Components/Molecules/Breadcrumbs/Breadcrumbs.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "../../../styles/molecules/Breadcrumbs.scss";
+
+interface Breadcrumb {
+    breadcrumbLabel: string,
+    breadcrumbLink: string
+}
+
+interface Props {
+    breadcrumbs: Breadcrumb[]
+}
+
+export default class Breadcrumbs extends React.Component<Props> {
+    render() {
+        return (
+            <div className="container-fluid breadcrumbs">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12">
+                            <span>
+                                {this.props.breadcrumbs.map((bc: Breadcrumb, index) => {
+                                    return (
+                                        <React.Fragment key={index}>
+                                            <a href={bc.breadcrumbLink} className="breadcrumb">
+                                                <i className="las la-home xxs"></i>{bc.breadcrumbLabel}
+                                            </a>
+                                            {index <= this.props.breadcrumbs.length -1 &&
+                                                <span className="breadcrumb__spacer"><i className="las la-angle-right xxs"></i></span>
+                                            }
+                                        </React.Fragment>
+
+                                    )
+                                })}
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import Button from "./Components/Atoms/Button/Button";
 import Accordion from "./Components/Molecules/Accordion/Accordion";
+import Breadcrumbs from "./Components/Molecules/Breadcrumbs/Breadcrumbs";
 
-export { Accordion, Button };
+export { Accordion, Button, Breadcrumbs };
